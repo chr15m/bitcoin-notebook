7b487168fc8356d2974b731c5849d3b77c2001cb
Chris McCormick <chris@mccormick.cx>
2015-11-15 11:11:33 +0800

    Basic function for calling bitcoin-iterate and parsing out the results into a Python-friendly datastructure.

---
+++ bi.py
@@ -0,0 +1,27 @@
+from subprocess import Popen, PIPE
+import os
+import re
+
+types = {
+    "%bl": int,
+    "%bN": int,
+}
+
+def bi(cmd):
+    # extract the fieldnames and set up a structure to store everything
+    fieldnames = [p.split(",") for p in cmd.split(" ") if "%" in p].pop()
+    fields = [(fn, []) for fn in fieldnames]
+    # run the external command to parse
+    cmd = os.path.dirname(__file__) + "/bitcoin-iterate/bitcoin-iterate -q " + cmd
+    stdout, stderr = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True).communicate()
+    # parse the results into our datastructure with python friendly values
+    for l in stdout.split("\n"):
+        if l:
+            vals = l.split(",")
+            if vals:
+                for f in range(len(fields)):
+                    fields[f][1].append(types.get(fields[f][0], str)(vals[f]))
+    return {
+        "results": dict(fields),
+        "stderr": stderr
+    }
